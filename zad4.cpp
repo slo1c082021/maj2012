06d57fa38463055b9d269289f89b9821671493d5
slo1c082021 <81389890+slo1c082021@users.noreply.github.com>
2022-03-30 08:07:02 +0200

    Add files via upload

---
+++ zad4.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    fstream file("cyfry.txt");
+    int s;
+    int parzyste = 0;
+    while(file >> s){
+            if(a%2 ==0)
+            parzyste ++;
+
+    }
+    cout << parzyste << endl;
+}
